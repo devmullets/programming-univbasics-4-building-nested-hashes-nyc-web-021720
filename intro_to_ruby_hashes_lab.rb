6225729388097499a765f5a332b557fa51f86458
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-04 16:36:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :key1 => 12,
+        :names => 12,
         :key2 => 13,
         :key3 => 14,
         :key4 => 15
