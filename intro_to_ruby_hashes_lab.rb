1866b5bc4a5a928f694de2550819b44cbc7bc0db
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-04 17:30:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :names3 => 3,
+        :b_and_o_railroad => {},
         :names4 => 4
       }
     }
